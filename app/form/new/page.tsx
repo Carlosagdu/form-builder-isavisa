62f939f16cf7990de7317e75bd11593392162086
Carlos Aguilar <carlosaguilarduarte0820@gmail.com>
2026-02-14 15:53:58 -0500

    Add-suspense-wrapper

--- app/form/new/page.tsx
+++ app/form/new/page.tsx
@@ -1,5 +1,11 @@
+import { Suspense } from "react"
+
 import { NewFormHeader } from "@/components/form/new/new-form-header"
 
 export default function NewFormPage() {
-  return <NewFormHeader />
+  return (
+    <Suspense fallback={<div className="h-full w-full bg-white" />}>
+      <NewFormHeader />
+    </Suspense>
+  )
 }
